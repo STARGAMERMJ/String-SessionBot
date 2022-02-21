4b4d1da409adbec8543174ff4d8617cde5f2bc2e
STARGAMERMJ <99683540+STARGAMERMJ@users.noreply.github.com>
2022-02-21 16:47:27 +0600

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -21,10 +21,10 @@ from telethon.errors import (
     PasswordHashInvalidError
 )
 
-ERROR_MESSAGE = "Oops! Pengecualian terjadi ! \n\n**Error** : {} " \
-            "\n\nTolong Laporan ke @UserbotTelegramSupport jika eror " \
-            "informasi sensitif dan Anda jika ingin melaporkan ini sebagai " \
-            "pesan kesalahan ini tidak dicatat oleh kami !"
+ERROR_MESSAGE = "Oops! Exception occurred ! \n\n**Error** : {} " \
+            "\n\nPlease Report to @scripthelper360 if there is an error " \
+            "If you want to report this, please do so as " \
+            "This error message was not logged by us. !"
 
 
 @Client.on_message(filters.private & ~filters.forwarded & filters.command('generate'))
@@ -39,25 +39,25 @@ async def main(_, msg):
 
 
 async def generate_session(bot, msg, telethon=False):
-    await msg.reply("__Memulai {} Session Generation...__".format("Telethon" if telethon else "Pyrogram"))
+    await msg.reply("__Start {} Session Generation...__".format("Telethon" if telethon else "Pyrogram"))
     user_id = msg.chat.id
-    api_id_msg = await bot.ask(user_id, 'Tolong Kirimkan  `API_ID` Anda Disini', filters=filters.text)
+    api_id_msg = await bot.ask(user_id, 'Please Submit Your `API_ID` Here', filters=filters.text)
     if await cancelled(api_id_msg):
         return
     try:
         api_id = int(api_id_msg.text)
     except ValueError:
-        await api_id_msg.reply('API_ID Tidak Benar , Tolong mulai ulang generating session lagi', quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await api_id_msg.reply('Incorrect API_ID, Please restart generating session again', quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
-    api_hash_msg = await bot.ask(user_id, 'Tolong Kirimkan `API_HASH` Anda Disini', filters=filters.text)
+    api_hash_msg = await bot.ask(user_id, 'Please Submit Your `API_HASH` Here', filters=filters.text)
     if await cancelled(api_id_msg):
         return
     api_hash = api_hash_msg.text
-    phone_number_msg = await bot.ask(user_id, 'Sekarang Kirimkan Nomor Telepon Anda Dengan Kode Nomor Negara\n\n**Contoh :** `+628xxxxxxx`', filters=filters.text)
+    phone_number_msg = await bot.ask(user_id, 'Now Submit Your Phone Number With Country Code\n\n**Example :** `+880xxxxxxx`', filters=filters.text)
     if await cancelled(api_id_msg):
         return
     phone_number = phone_number_msg.text
-    await msg.reply("```Mengirimkan Kode OTP...```")
+    await msg.reply("```Sending OTP Code...```")
     if telethon:
         client = TelegramClient(StringSession(), api_id, api_hash)
     else:
@@ -69,17 +69,17 @@ async def generate_session(bot, msg, telethon=False):
         else:
             code = await client.send_code(phone_number)
     except (ApiIdInvalid, ApiIdInvalidError):
-        await msg.reply('`API_ID` dan `API_HASH` masih salah , Tolong mulai ulang generating session lagi', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await msg.reply('`API_ID` and `API_HASH` are still wrong , Please restart generating session again', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     except (PhoneNumberInvalid, PhoneNumberInvalidError):
-        await msg.reply('Nomor Telepon masih salah , Tolong mulai ulang generating lagi', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await msg.reply('The phone number is still wrong, please restart generating again', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     try:
-        phone_code_msg = await bot.ask(user_id, "Tolong cek untuk kode OTP dalam akun Telegram , Jika anda mendapatkannya , Kirim kode OTP disini sesudah membaca format di bawah ini\nJika kode OTP adalah `12345` , **Dan kirim dengan** `1 2 3 4 5` pakai spasi", filters=filters.text, timeout=600)
+        phone_code_msg = await bot.ask(user_id, "Please check for the OTP code in your Telegram account, if you get it, send the OTP code here after reading the format below\nIf the OTP code is `12345` , **And send with** `1 2 3 4 5` use space", filters=filters.text, timeout=600)
         if await cancelled(api_id_msg):
             return
     except TimeoutError:
-        await msg.reply('Batas waktu tercapai 10 menit , Tolong mulai ulang generating session lagi', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await msg.reply('Time limit reached 10 minutes, Please restart generating session again', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     phone_code = phone_code_msg.text.replace(" ", "")
     try:
@@ -88,16 +88,16 @@ async def generate_session(bot, msg, telethon=False):
         else:
             await client.sign_in(phone_number, code.phone_code_hash, phone_code)
     except (PhoneCodeInvalid, PhoneCodeInvalidError):
-        await msg.reply('Kode OTP ini salah , Tolong mulai ulang generating session lagi', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await msg.reply('This OTP code is wrong, Please restart the generating session again', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     except (PhoneCodeExpired, PhoneCodeExpiredError):
-        await msg.reply('Kode OTP ini kadaluarsa , Tolong mulai ulang generating session lagi', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await msg.reply('This OTP code has expired, Please restart generating session again', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     except (SessionPasswordNeeded, SessionPasswordNeededError):
         try:
-            two_step_msg = await bot.ask(user_id, 'Akun Anda telah mengaktifkan verifikasi dua langkah , Tolong berikan kata sandinya', filters=filters.text, timeout=300)
+            two_step_msg = await bot.ask(user_id, 'Your account has enabled two-step verification, Please provide the password', filters=filters.text, timeout=300)
         except TimeoutError:
-            await msg.reply('Batas waktu tercapai 5 menit , Tolong mulai ulang generating session lagi', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+            await msg.reply('Time limit reached 5 minutes, Please restart generating session again', reply_markup=InlineKeyboardMarkup(Data.generate_button))
             return
         try:
             password = two_step_msg.text
@@ -108,16 +108,16 @@ async def generate_session(bot, msg, telethon=False):
             if await cancelled(api_id_msg):
                 return
         except (PasswordHashInvalid, PasswordHashInvalidError):
-            await two_step_msg.reply('Kata sandi salah , Tolong mulai ulang generating session lagi', quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
+            await two_step_msg.reply('Wrong password, Please restart generating session again', quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
             return
     if telethon:
         string_session = client.session.save()
     else:
         string_session = await client.export_session_string()
-    text = "**{} STRING SESSION** \n\n`{}` \n\nGenerating String Session Bot\n**Support :** @UserbotTelegramSupport".format("TELETHON" if telethon else "PYROGRAM", string_session)
+    text = "**{} STRING SESSION** \n\n`{}` \n\nGenerating String Session Bot\n**Support :** @scripthelper360".format("TELETHON" if telethon else "PYROGRAM", string_session)
     await client.send_message("me", text)
     await client.disconnect()
-    await phone_code_msg.reply("Berhasil Megambil {} string session\n\nSilahkan cek di Pesan Tersimpan\n\nJika ada masalah laporkan ke @tzypis".format("telethon" if telethon else "pyrogram"))
+    await phone_code_msg.reply("Successfully Picked Up {} string session\n\nPlease check the Saved Messages\n\nIf there is a problem report it to @STARGAMERMJ".format("telethon" if telethon else "pyrogram"))
 
 
 async def cancelled(msg):
